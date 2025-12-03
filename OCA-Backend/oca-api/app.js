d4fa5274b347ff3030a5e543376f3ac6ea9a5aaa
simulati0n <tomthen4@gmail.com>
2025-12-03 11:07:59 -0500

    Add /model endpoint for Ollama cloud LLM streaming

--- OCA-Backend/oca-api/app.js
+++ OCA-Backend/oca-api/app.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,9 +7,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var modelRouter = require('./routes/model');
 
 var app = express();
 
+dotenv.config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/model',modelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
